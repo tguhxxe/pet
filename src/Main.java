cdc018461679a11170c80321d776c074d4045d36
tguhxxe <bangguh18@gmail.com>
2024-07-04 17:59:29 +0700

    teguh setia

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+       Pet pet = new Pet();
+       pet.displayPet();
+    }
+}
---
+++ src/Pet.java
@@ -0,0 +1,15 @@
+public class Pet {
+    String nama = "Albert";
+    int umur = 7;
+    String ras = "Corgi";
+    String makanan = "Dry Food";
+    String warna = "Kuning";
+
+   public void displayPet() {
+       System.out.println("Nama: " + nama);
+       System.out.println("Umur: " + umur);
+       System.out.println("Ras: " + ras);
+       System.out.println("Makanan: " + makanan);
+       System.out.println("Warna: " + warna);
+   }
+}
